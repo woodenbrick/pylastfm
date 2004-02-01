1e7a28012a00901dd49b8183e42823e9f18659be
Daniel Woodhouse <wodemoneke@gmail.com>
2004-02-01 00:54:42 +0000

    created artist and auth classes

---
+++ pylastfm/api/artist.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+class ArtistMethod(object):
+    def __init__(self, conn):
+        self.conn = conn
+        
+    def artist_addTags():
+        pass
+    
+    def artist_getEvents():
+        pass
+    
+    def artist_getImages():
+        pass
+    
+    def artist_getInfo():
+        pass
+    
+    def artist_getPastEvents():
+        pass
+    
+    def artist_getPodcast():
+        pass
+    
+    def artist_getShouts():
+        pass
+    
+    def artist_getSimilar():
+        pass
+    
+    def artist_getTags():
+        pass
+    
+    def artist_getTopAlbums():
+        pass
+    
+    def artist_getTopFans():
+        pass
+    
+    def artist_getTopTags():
+        pass
+    def artist_getTopTracks():
+        pass
+    def artist_removeTag():
+        pass
+    
+    def artist_search():
+        pass
+    
+    def artist_share():
+        pass
+    
+    def artist_shout():
+        pass
