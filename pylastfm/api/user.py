18f8a22a45dca7ed108e41b550f83dad9cb6a279
Daniel Woodhouse <wodemoneke@gmail.com>
2009-11-16 00:39:10 +0000

    added user method skeleton and documentation

--- pylastfm/api/user.py
+++ pylastfm/api/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from error import LastfmError, LastfmParamError, LastfmAuthenticationError
-from _basetype import AbstractType
+from _basetype import AbstractType, AbstractMethod
 
 class User(AbstractType):
     ROOT_NODE = "user"
@@ -57,41 +57,266 @@ class User(AbstractType):
     
 
 
-class UserMethod(object):
+class UserMethod(AbstractMethod):
     def __init__(self, conn):
         self.conn = conn
+    
+
+    
+    def getEvents(self, user=None):
+        """
+        Fetch the events this user is attending. Authentication not required.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session
+        @return: A list of L{Event} objects
+        """
+        pass
+    
+    def getFriends(self, user=None, recenttracks=False, limit=50, page=1):
+        """
+        Fetch the friends of this user. Authentication not required.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session
+        @param recenttracks: (Optional) Whether or not to include information
+        about friends latest tracks.
+        @param limit: (Optional) Limit the amount of friends to fetch.
+        @param page: (Optional) The page number to fetch.
+        @return: A list of L{User} objects
+        """
+        pass
+    
+    
+    def getInfo(self, user=None):
+        """
+        Get information about a user. Authentication not required.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session
+        @raise LastfmError : if user name is not set. 
+        @return: A L{User} object
+        """
+        user = self._getUsername(user)
+        xml = self.conn._api_get_request(user=user, method="user.getInfo")
+        return self.conn.create_objects(xml, User)  
+    
     
     def getLovedTracks(self, user=None, limit=None, page=None):
         """
         Get the last 50 tracks loved by a user. Authentication not required.
-        @param user: (Optional) The user name to fetch the loved tracks for.
-        If left blank the username for this session will be used.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session
         @param limit: (Optional) An integer used to limit the number of tracks
         returned per page. The default is 50.
         @param page: (Optional) The page number to fetch.
         @raise LastfmError : if user name is not set
         @return: A list of L{Track} objects
         """
-        if user is None:
-            if self.conn.username is None:
-                raise LastfmError("Username not set")
-            user = self.conn.username
+        user = self._getUsername(user)
         xml = self.conn._api_get_request(user=user, method="user.getLovedTracks")
         return self.conn.create_objects(xml, Track)
     
     
-    def getInfo(self, user=None):
+    def getNeighbours(self, user=None, limit=None):
         """
-        Get information about a user. Authentication not required.
-        @param user: (Optional) The user name to fetch info for.
-        If left blank the username for this session will be used.
-        @raise LastfmError : if user name is not set. 
-        @return: A L{User} object
+        Get the neighbours for this user.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session
+        @param limit: (Optional) n integer used to limit the number of neighbours
+        fetched.
+        @return: A list of L{User} objects
         """
-        if user is None:
-            if self.conn.username is None:
-                print 'not set'
-                #raise LastfmError("Username not set")
-            user = self.conn.username
-        xml = self.conn._api_get_request(user=user, method="user.getInfo")
-        return self.conn.create_objects(xml, User)  
+        pass
+    
+    def getPastEvents(self, user=None, page=None, limit=None):
+        """
+        A list of events the user has attended in the past. Authentication not
+        required.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session
+        @param page: (Optional) The page number to scan to.
+        @param: limit (Optional) The maximum number of events to return per page.
+        @return: A list of L{Event} objects
+        """
+        pass
+    
+    def getPlaylists(self, user=None):
+        """
+        Get a list of a user's playlists on Last.fm. Authentication not required.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session
+        @return: A list of L{Playlist} objects.
+        """
+        pass
+    
+    def getRecentStations(self, user=None, limit=10, page=None):
+        """
+        Get a list of the recent Stations listened to by this user.
+        Authentication required.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session.
+        @param limit: (Optional) An integer used to limit the number of
+        stations returned per page. The default is 10, the maximum is 25.
+        @param page: (Optional) The page number to fetch.
+        @return a list of L{Station} objects.
+        """
+        #make sure limit is under 25
+        pass
+    
+    def getRecentTracks(self, user=None, limit=None, page=None):
+        """
+        Get a list of the recent tracks listened to by this user. Also
+        includes the currently playing track with the nowplaying="true"
+        attribute if the user is currently listening. This service does not 
+        require authentication.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session
+        @param limit: (Optional) An integer used to limit the number of
+        tracks returned.
+        @param page: (Optional) An integer used to fetch a specific page of tracks.
+        @return: A list of L{Track} objects
+        """
+        pass
+    
+    def getRecommendedArtists(self, page=None, limit=None):
+        """
+        Get Last.fm artist recommendations for a user. This service requires
+        authentication.
+        @param page: The page to scan to.
+        @param limit: The number of artists to return per page.
+        @return: A list of L{Artist} objects
+        """
+        pass
+        
+    def getRecommendedEvents(self, page=None, limit=None):
+        """
+        Get a paginated list of all events recommended to a user by Last.fm,
+        based on their listening profile. This service requires authentication.
+        @param page: (Optional) The page number to scan to.
+        @param limit: (Optional) The number of events to return per page.
+        @return: A list of L{Event} objects.
+        """
+        pass
+    
+    def getShouts(self, user=None):
+        """
+        Get shouts for this user. Also available as an rss feed. This service
+        does not require authentication.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session.
+        @return: A list of L{Shout} objects.
+        """
+        pass
+    
+    def getTopAlbums(self, user=None, period="overall"):
+        """
+        Get the top albums listened to by a user. You can stipulate a time 
+        period. Sends the overall chart by default. This service does not
+        require authentication.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session
+        @param period: (Optional) overall | 7day | 3month | 6month | 12month
+        - The time period over which to retrieve top albums for.
+        @return: A list of L{Album} objects.
+        """
+        pass
+    
+    def getTopArtists(self, user=None, period="overall"):
+        """
+        Get the top artists listened to by a user. You can stipulate a time
+        period. Sends the overall chart by default. This service does not
+        require authentication.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session.
+        @param period: (Optional) overall | 7day | 3month | 6month | 12month
+        - The time period over which to retrieve top artists for.
+        """
+        pass
+    
+    def getTopTags(self, user=None, limit=None):
+        """
+        Get the top tags used by this user. This service does not require
+        authentication.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session.
+        @param limit: (Optional) Limit the number of tags returned
+        @return: A list of L{Tag} objects.
+        """
+        pass
+    
+    def getTopTracks(self, user=None, period="overall"):
+        """
+        Get the top tracks listened to by a user. You can stipulate a time
+        period. Sends the overall chart by default. This service does not
+        require authentication.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session.
+        period (Optional) : overall | 7day | 3month | 6month | 12month -
+        The time period over which to retrieve top tracks for.
+        @return: A list of L{Track} objects.
+        """
+        pass
+    
+    def getWeeklyAlbumChart(self, user=None, _from=None, to=None):
+        """
+        Get an album chart for a user profile, for a given date range.
+        If no date range is supplied, it will return the most recent album
+        chart for this user. This service does not require authentication.
+         @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session.
+        @param _from: (Optional) The date at which the chart should start from.
+        See L{User.getChartsList()} for more.
+        @param to: (Optional) : The date at which the chart should end on.
+        See L{User.getChartsList()} for more.
+        @return: A list of L{Album} objects
+        """
+        pass
+    
+    def getWeeklyArtistChart(self, user=None, _from=None, to=None):
+        """
+        Get an artist chart for a user profile, for a given date range.
+        If no date range is supplied, it will return the most recent artist
+        chart for this user. This service does not require authentication.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session.
+        @param _from: (Optional) The date at which the chart should start from.
+        See L{User.getWeeklyChartsList()} for more.
+        @param to: (Optional) : The date at which the chart should end on.
+        See L{User.getWeeklyChartsList()} for more.
+        @return: A list of L{Artist} objects
+        """
+        pass
+    
+    def getWeeklyChartList(self, user=None):
+        """
+        Get a list of available charts for this user, expressed as date ranges
+        which can be sent to the chart services. This service does not require
+        authentication.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session.
+        @return: A list of L{Chart} objects
+        """
+        pass
+
+    def getWeeklyTrackChart(self, user=None, _from=None, to=None):
+        """
+        Get a track chart for a user profile, for a given date range.
+        If no date range is supplied, it will return the most recent track
+        chart for this user. This service does not require authentication.
+        @param user: (Optional) A string of the user to fetch results for,
+        a L{User} object or None for user of the current session.
+        @param _from: (Optional) The date at which the chart should start from.
+        See L{User.getWeeklyChartsList()} for more.
+        @param to: (Optional) : The date at which the chart should end on.
+        See L{User.getWeeklyChartsList()} for more.
+        @return: A list of L{Track} objects
+        """
+        pass
+
+    def shout(self, user, message):
+        """
+        Shout on this user's shoutbox. This service requires authentication.
+        @param user: (Optional) A string of the user to to shout on or 
+        a L{User} object.
+        @param message: (Optional) The message to post to the shoutbox.
+        @return: True if posting was successful.
+        """
+        pass
